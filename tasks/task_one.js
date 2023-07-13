632446892fd3a3451fc44ea02d1b5b403aa8dcba
donvico <wvictoryc@gmail.com>
2023-07-13 12:49:36 +0100

    commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,8 @@
 
-// Write something
+// task one
+function js_style (){
+    let changeStyle = document.getElementById('text')
+    changeStyle.style.color = 'red'
+    changeStyle.style.fontSize = '20px'
+    changeStyle.style.fontFamily = 'vedana'
+}
